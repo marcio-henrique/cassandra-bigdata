168a7d96cc073977fc9ee1124c6ac976befc47f9
Márcio Henrique <marcioh.ikke@gmail.com>
2023-04-04 16:01:01 -0300

    import data

--- 911-crud.py
+++ 911-crud.py
@@ -7,12 +7,9 @@ session = cluster.connect()
 
 
 # Cria o keyspace e a tabela
-session.execute("CREATE KEYSPACE IF NOT EXISTS 911_calls \
-                  WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}")
+session.execute("CREATE KEYSPACE IF NOT EXISTS 911_calls \ WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}")
 
-
-session.execute("CREATE TABLE IF NOT EXISTS 911_calls.calls \
-                  (call_id int, time timestamp, location text, description text, PRIMARY KEY (call_id))")
+session.execute("CREATE TABLE IF NOT EXISTS 911_calls.calls \ (call_id int, time timestamp, location text, description text, PRIMARY KEY (call_id))")
 
 
 # Lê o arquivo CSV e insere os dados na tabela
@@ -24,13 +21,11 @@ with open('911.csv') as f:
         time = data[1]
         location = data[4]
         description = data[5]
-        session.execute(f"INSERT INTO 911_calls.calls (call_id, time, location, description) \
-                          VALUES ({call_id}, '{time}', '{location}', '{description}')")
+        session.execute(f"INSERT INTO 911_calls.calls (call_id, time, location, description) \ VALUES ({call_id}, '{time}', '{location}', '{description}')")
 
 
 # Consulta os dados da tabela
 rows = session.execute("SELECT * FROM 911_calls.calls WHERE location = 'REAR 2131 WOODEN SHOE CT'")
-
 
 for row in rows:
     print(row.call_id, row.time, row.location, row.description)
