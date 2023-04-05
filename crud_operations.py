e6ac9ab5c268218e3e238c924e360bc3519ab438
Márcio Henrique <marcioh.ikke@gmail.com>
2023-04-05 09:09:23 -0300

    crud operations

--- crud_operations.py
+++ crud_operations.py
@@ -5,42 +5,60 @@ from cassandra.cluster import Cluster
 cluster = Cluster(['localhost'])
 session = cluster.connect()
 
+print('CONSULTANDO DADOS')
 # Consulta os dados da tabela
-rows = session.execute("SELECT * FROM detran.calls LIMIT 10 ALLOW FILTERING")
-
-for row in rows:
-    print(row.call_id, row.time, row.location)
+results = session.execute("SELECT * FROM detran.calls LIMIT 10 ALLOW FILTERING")
+for result in results:
+    print('[', result.call_id, ',', result.time, ',', result.location, ']')
 
 # Cria um registro
-
 call_id_test = uuid.uuid4()
 time_test = '04/04/2023'
 location_test = 'California'
 
+print('\n')
 print('INSERINDO REGISTRO')
 session.execute(f"""INSERT INTO detran.calls (call_id, time, location) 
         VALUES ({call_id_test}, '{time_test}', '{location_test}')""")
 
 # Consulta o registro
-
+print('\n')
 print('CONSULTANDO REGISTRO INSERIDO')
-session.execute(f"""SELECT * FROM detran.calls WHERE (call_id = '{call_id_test}')""")
-
+results = session.execute(f"""SELECT * FROM detran.calls WHERE (call_id = {call_id_test})""")
+for result in results:
+        print('[', result.call_id, ',', result.time, ',', result.location, ']') 
 
 # Atualizando o registro
 
 new_location = 'Boston'
 
+print('\n')
 print('ATUALIZANDO O REGISTRO')
-session.execute(f"""UPDATE detran.calls SET location = '{new_location}' 
-        WHERE (call_id = '{call_id_test}')""")
+results = session.execute(f"""UPDATE detran.calls SET location = '{new_location}' 
+        WHERE (call_id = {call_id_test})""")
+print(results)
+
+# Consulta o registro
+print('\n')
+print('CONSULTANDO REGISTRO INSERIDO')
+results = session.execute(f"""SELECT * FROM detran.calls WHERE (call_id = {call_id_test})""")
+for result in results:
+        print('[', result.call_id, ',', result.time, ',', result.location, ']')
 
 
 # Remove o registro
-
+print('\n')
 print('REMOVENDO REGISTRO')
-session.execute(f"""DELETE FROM detran.calls WHERE (call_id = '{call_id_test}')""")
+session.execute(f"""DELETE FROM detran.calls WHERE (call_id = {call_id_test})""")
 
 # Encerra a conexão com o cluster Cassandra
+
+# Consulta o registro
+print('\n')
+print('CONSULTANDO REGISTRO INSERIDO')
+results = session.execute(f"""SELECT * FROM detran.calls WHERE (call_id = {call_id_test})""")
+for result in results:
+        print('[', result.call_id, ',', result.time, ',', result.location, ']')
+
 session.shutdown()
 cluster.shutdown()
