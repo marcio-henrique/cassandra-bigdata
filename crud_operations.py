c2b07ab889755b56bb5d5349a153684af24c6cc1
Michael Miller <41575314+Miller202@users.noreply.github.com>
2023-04-05 08:11:09 -0300

    insert, get and remove operations

--- crud_operations.py
+++ crud_operations.py
@@ -1,5 +1,5 @@
+import uuid
 from cassandra.cluster import Cluster
-import pandas as pd
 
 # Conecta ao cluster Cassandra
 cluster = Cluster(['localhost'])
@@ -10,6 +10,23 @@ rows = session.execute("SELECT * FROM detran.calls LIMIT 10 ALLOW FILTERING")
 
 for row in rows:
     print(row.call_id, row.time, row.location)
+
+# Cria um registro
+
+call_id_test = uuid.uuid4()
+time_test = '04/04/2023'
+location_test = 'California, Av 32'
+
+session.execute(f"""INSERT INTO detran.calls (call_id, time, location) 
+        VALUES ({call_id_test}, '{time_test}', '{location_test}')""")
+
+# Consulta o registro
+
+session.execute(f"""SELECT * FROM detran.calls WHERE (call_id = '{call_id_test}')""")
+
+# Remove o registro
+
+session.execute(f"""DELETE FROM detran.calls WHERE (call_id = '{call_id_test}')""")
 
 # Encerra a conexão com o cluster Cassandra
 session.shutdown()
