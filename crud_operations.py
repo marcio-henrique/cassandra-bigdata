a69eabf53d8ee69db74d61b2b969e91a2417ed9e
Michael Miller <41575314+Miller202@users.noreply.github.com>
2023-04-05 08:23:09 -0300

    Update crud_operations.py

--- crud_operations.py
+++ crud_operations.py
@@ -15,17 +15,30 @@ for row in rows:
 
 call_id_test = uuid.uuid4()
 time_test = '04/04/2023'
-location_test = 'California, Av 32'
+location_test = 'California'
 
+print('INSERINDO REGISTRO')
 session.execute(f"""INSERT INTO detran.calls (call_id, time, location) 
         VALUES ({call_id_test}, '{time_test}', '{location_test}')""")
 
 # Consulta o registro
 
+print('CONSULTANDO REGISTRO INSERIDO')
 session.execute(f"""SELECT * FROM detran.calls WHERE (call_id = '{call_id_test}')""")
+
+
+# Atualizando o registro
+
+new_location = 'Boston'
+
+print('ATUALIZANDO O REGISTRO')
+session.execute(f"""UPDATE detran.calls SET location = '{new_location}' 
+        WHERE (call_id = '{call_id_test}')""")
+
 
 # Remove o registro
 
+print('REMOVENDO REGISTRO')
 session.execute(f"""DELETE FROM detran.calls WHERE (call_id = '{call_id_test}')""")
 
 # Encerra a conexão com o cluster Cassandra
